71a59b2ab243aca759a49f1226f4015ddd804a11
Łaurent ʘ❢Ŧ Ŧough <LaurentFough@users.noreply.github.com>
2021-05-02 22:15:50 -0700

    Update proxyswitch.sh
    - update exec env
    - update function names

--- proxyswitch.sh
+++ proxyswitch.sh
@@ -1,113 +1,113 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-# Source the Proxy Database
-source "$HOME/.proxyswitch/proxyDB.txt"
+## Source the Proxy Database
+source "$HOME/.config/proxyswitch/proxyDB.txt"
 
-# Proceed only if root privileges
-CheckRoot(){
+## Proceed only if root privileges
+checkRoot(){
 	if [ $EUID -ne 0 ]; then
-		echo "Do you have proper administration rights? (super-user?)"
-		echo "Root privileges are required."
+		echo "[proxySwitch] Do you have proper administration rights? (super-user?)"
+		echo "[proxySwitch] Root privileges are required."
 		exit
 	else
-		ProxyChoice
+		proxyChoice
 		return 0
 	fi
 }
 
-# ask user which proxy he wants to use
-ProxyChoice(){
+## ask user which proxy he wants to use
+proxyChoice(){
 	clear
-	echo "You have  $PROXYCOUNT  saved proxies."
+	echo "[proxySwitch] You have $PROXYCOUNT saved proxies."
 	echo
 
-	echo "0 > USE NO PROXY"
+	echo "[proxySwitch] 0 > USE NO PROXY"
 	echo
 
 	for (( i = 1; i <= $PROXYCOUNT; i++ )); do
 		proxy="${PROXIES[$i-1]}"
 	
-		# display choice number
-		echo -n "$i > "
+		## display choice number
+		echo -n "[proxySwitch] $i > "
 	
-		# display proxy:port and username
+		## display proxy:port and username
 		sed 's/\(.*\):.*@\(.*\)/\2  \1/' <<< "$proxy"
 		echo
 	done
 
-	echo "$i > SOME OTHER PROXY"
+	echo "[proxySwitch] $i > SOME OTHER PROXY"
 	echo
 
-	read -p "Chose any one option : " ProxyChoice
+	read -p "[proxySwitch] Chose any one option : " proxyChoice
 	
-	# check if in range
-	if [[ $ProxyChoice -gt '0' && $ProxyChoice -le $PROXYCOUNT ]]; then
-		# ProxyChoice=$((ProxyChoice-1))
-		# Read the proxy details from the database
-		proxy="${PROXIES[$ProxyChoice-1]}"
-		# Set that proxy
-		SetProxy $proxy
-	elif [[ $ProxyChoice == $(($PROXYCOUNT + 1)) ]]; then
-		# Set a new Proxy
-		NewProxy
-	elif [[ $ProxyChoice == '0' ]]; then
-		# No Proxy
-		ProxyNone 'none'
+	## check if in range
+	if [[ $proxyChoice -gt '0' && $proxyChoice -le $PROXYCOUNT ]]; then
+		# proxyChoice=$((proxyChoice-1))
+		## Read the proxy details from the database
+		proxy="${PROXIES[$proxyChoice-1]}"
+		## Set that proxy
+		setProxy $proxy
+	elif [[ $proxyChoice == $(($PROXYCOUNT + 1)) ]]; then
+		## Set a new Proxy
+		newProxy
+	elif [[ $proxyChoice == '0' ]]; then
+		## No Proxy
+		proxyNone 'none'
 	else
-		echo "Invalid Proxy Selected."
-		echo "ProxySwitch Failed."
+		echo "[proxySwitch] Invalid Proxy Selected."
+		echo "[proxySwitch] proxySwitch Failed."
 	fi
 }
 
-# Calls functions to set proxy in different fields
-SetProxy(){
-	# Remove all the previous Proxy Settings
-	ProxyNone
+## Calls functions to set proxy in different fields
+setProxy(){
+	## Remove all the previous Proxy Settings
+	proxyNone
 
-	# System Settings Proxy 
-	ProxySYS $1
-	# Apt Proxy Configuration
-	ProxyAPT $1
-	# Environment variables set up
-	ProxyENV $1
-	# exporting in .bashrc file
-	ProxyBASHRC $1
+	## System Settings Proxy 
+	proxySYS $1
+	## Apt Proxy Configuration
+	proxyAPT $1
+	## Environment variables set up
+	proxyENV $1
+	## exporting in .bashrc file
+	proxyBASHRC $1
 
 	source $HOME/.bashrc
 	
-	echo "New proxy settings applied."
-	echo "ProxySwitch Successful."
+	echo "[proxySwitch] New proxy settings applied."
+	echo "[proxySwitch] proxySwitch Successful."
 }
 
-# set the System proxy
-ProxySYS(){
+## set the System proxy
+proxySYS(){
 	proxy="$1"
 	proxy=$(sed 's/.*@\(.*\)/\1/' <<< "$proxy")
-	ProxyPROXY=$(sed 's/\(.*\):.*/\1/' <<< "$proxy")
-	ProxyPORT=$(sed 's/.*:\(.*\)/\1/' <<< "$proxy")
+	proxyPROXY=$(sed 's/\(.*\):.*/\1/' <<< "$proxy")
+	proxyPORT=$(sed 's/.*:\(.*\)/\1/' <<< "$proxy")
 
 	sudo gsettings set org.gnome.system.proxy mode 'manual';
-	sudo gsettings set org.gnome.system.proxy.http host $ProxyPROXY;
-	sudo gsettings set org.gnome.system.proxy.http port $ProxyPORT;
-	sudo gsettings set org.gnome.system.proxy.https host $ProxyPROXY
-	sudo gsettings set org.gnome.system.proxy.https port $ProxyPORT
-	sudo gsettings set org.gnome.system.proxy.ftp host $ProxyPROXY
-	sudo gsettings set org.gnome.system.proxy.ftp port $ProxyPORT
-	sudo gsettings set org.gnome.system.proxy.socks host $ProxyPROXY
-	sudo gsettings set org.gnome.system.proxy.socks port $ProxyPORT
-	sudo gsettings set org.gnome.system.proxy.all host $ProxyPROXY
-	sudo gsettings set org.gnome.system.proxy.all port $ProxyPORT
+	sudo gsettings set org.gnome.system.proxy.http host $proxyPROXY;
+	sudo gsettings set org.gnome.system.proxy.http port $proxyPORT;
+	sudo gsettings set org.gnome.system.proxy.https host $proxyPROXY
+	sudo gsettings set org.gnome.system.proxy.https port $proxyPORT
+	sudo gsettings set org.gnome.system.proxy.ftp host $proxyPROXY
+	sudo gsettings set org.gnome.system.proxy.ftp port $proxyPORT
+	sudo gsettings set org.gnome.system.proxy.socks host $proxyPROXY
+	sudo gsettings set org.gnome.system.proxy.socks port $proxyPORT
+	sudo gsettings set org.gnome.system.proxy.all host $proxyPROXY
+	sudo gsettings set org.gnome.system.proxy.all port $proxyPORT
 }
 
-# set the apt proxy
-ProxyAPT(){
+## set the apt proxy
+proxyAPT(){
 	proxy="$1"
 	
 	sudo echo -e "Acquire::http::proxy \"http://$proxy/\";\nAcquire::https::proxy \"https://$proxy/\";\nAcquire::ftp::proxy \"ftp://$proxy/\";\nAcquire::socks::proxy \"socks://$proxy/\";\nAcquire::all::proxy \"https://$proxy/\";" >> /etc/apt/apt.conf
 }
 
-# set up the environment variables in the proxy
-ProxyENV(){
+## set up the environment variables in the proxy
+proxyENV(){
 	proxy="$1"
 	
 	sudo echo -e "http_proxy=\"http://$proxy/\"\nhttps_proxy=\"https://$proxy/\"\nftp_proxy=\"ftp://$proxy/\"\nsocks_proxy=\"socks://$proxy/\"\nall_proxy=\"https://$proxy/\"" >> /etc/environment
@@ -119,21 +119,21 @@ ProxyENV(){
 	export all_proxy="https://$proxy/"
 }
 
-# exporting the variables in the bashrc file.
-ProxyBASHRC(){
+## exporting the variables in the bashrc file.
+proxyBASHRC(){
 	proxy="$1"
 
 	sudo echo -e "## Proxy settings by proxyswitch\nexport http_proxy=\"http://$proxy/\"\nexport https_proxy=\"https://$proxy/\"\nexport socks_proxy=\"socks://$proxy/\"\nexport ftp_proxy=\"ftp://$proxy/\"\nexport all_proxy=\"https://$proxy/\"" >> $HOME/.bashrc
 }
 
-# Set no proxy ... Remove all the previous proxy settings 
-ProxyNone(){
-	# System Settings Proxy
+## Set no proxy ... Remove all the previous proxy settings 
+proxyNone(){
+	## System Settings Proxy
 	if [[ $1 == 'none' ]]; then
 		sudo gsettings set org.gnome.system.proxy mode 'none'
 	fi
 
-	# Apt Proxy Configuration
+	## Apt Proxy Configuration
 	sudo sed -i.bak '/http::proxy/d' /etc/apt/apt.conf
 	sudo sed -i.bak '/https::proxy/d' /etc/apt/apt.conf
 	sudo sed -i.bak '/socks::proxy/d' /etc/apt/apt.conf
@@ -141,45 +141,45 @@ ProxyNone(){
 	sudo sed -i.bak '/all::proxy/d' /etc/apt/apt.conf
 	
 
-	# Environment variables set up
+	## Environment variables set up
 	sudo sed -i.bak '/http_proxy/d' /etc/environment
 	sudo sed -i.bak '/https_proxy/d' /etc/environment
 	sudo sed -i.bak '/ftp_proxy/d' /etc/environment
 	sudo sed -i.bak '/socks_proxy/d' /etc/environment
 	sudo sed -i.bak '/all_proxy/d' /etc/environment
 
-	# exporting in .bashrc file
-	sudo sed -i.bak '/proxyswitch/d' $HOME/.bashrc
+	## exporting in .bashrc file
+	sudo sed -i.bak '/proxySwitch/d' $HOME/.bashrc
 	sudo sed -i.bak '/http_proxy/d' $HOME/.bashrc
 	sudo sed -i.bak '/https_proxy/d' $HOME/.bashrc
 	sudo sed -i.bak '/ftp_proxy/d' $HOME/.bashrc
 	sudo sed -i.bak '/socks_proxy/d' $HOME/.bashrc
 	sudo sed -i.bak '/all_proxy/d' $HOME/.bashrc
 
-	echo "All previous proxy settings removed."
+	echo "[proxySwitch] All previous proxy settings removed."
 }
 
-# Set a new proxy not saved in the database 
-NewProxy(){
+## Set a new proxy not saved in the database 
+newProxy(){
 	echo
-	echo "Enter Details for proxy : "
-	read -p "Proxy (e.g. 202.141.80.24) : " proxy
-	read -p "Proxy Port (e.g. 3128) : " proxyPort
-	read -p "Use proxy Authentication? (Y/N) : " -n 1 response
+	echo "[proxySwitch] Enter Details for proxy : "
+	read -p "[proxySwitch] Proxy (e.g. 202.141.80.24) : " proxy
+	read -p "[proxySwitch] Proxy Port (e.g. 3128) : " proxyPort
+	read -p "[proxySwitch] Use proxy Authentication? (Y/N) : " -n 1 response
 	echo
 	case $response in
 		y|Y)
-			echo "Enter you proxy Authentication"
-			read -p "Enter Username : " -r proxyUsername
-			read -p "Enter Password : " -r proxyPassword
-			Proxy=$proxyUsername":"$proxyPassword"@"$proxy":"$proxyPort
+			echo "[proxySwitch] Enter you proxy Authentication"
+			read -p "[proxySwitch] Enter Username : " -r proxyUsername
+			read -p "[proxySwitch] Enter Password : " -r proxyPassword
+			proxy=$proxyUsername":"$proxyPassword"@"$proxy":"$proxyPort
 			;;
 		*)	
-			Proxy=$proxy":"$proxyPort
+			proxy=$proxy":"$proxyPort
 			;;
 	esac
 
-	SetProxy $Proxy
+	setProxy $Proxy
 }
 
-CheckRoot
+checkRoot
